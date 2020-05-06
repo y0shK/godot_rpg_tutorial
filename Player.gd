e083f4a951ae888812469059028c7ea060d0181b
Yash Karandikar <25213173+y0shK@users.noreply.github.com>
2020-05-05 21:13:06 -0700

    part 6 with animation player/tree

--- Player.gd
+++ Player.gd
@@ -5,7 +5,14 @@ const MAX_SPEED = 80
 const ACCELERATION = 500
 const FRICTION = 500
 
-onready var animationPlayer = $AnimationPlayer # calls a path to the node in the same scene
+onready var animationPlayer = $AnimationPlayer
+#var animationPlayer = null
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+#func _ready():
+	# animationPlayer var is a reference to the node of te same scene
+	#animationPlayer = $AnimationPlayer # path to a node in the same scene
 
 # underscore -> callback function - runs at runtime
 func _physics_process(delta):
@@ -15,16 +22,18 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		# moving
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		# just keep animationTree during movement, remembers motion direction when stopping
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		#velocity += input_vector * ACCELERATION * delta
 		#velocity = velocity.clamped(MAX_SPEED)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		animationPlayer.play("IdleRight")
+		#animationPlayer.play("IdleRight")
 	
+	#move_and_collide(velocity * delta)
 	velocity = move_and_slide(velocity) # in real-time frames, apply move_and_slide info to the velocity
